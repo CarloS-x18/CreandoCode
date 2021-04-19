db996b1af2d408f94f9747fbf21399c870c1fe1d
Carlos Alberto <carlosescobedomoreno@gmail.com>
2021-04-19 11:50:53 -0500

    Agregue las publicaciones al index

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,37 @@
-const menu = document.querySelector('.menu');
-
-menu.addEventListener('click', e => {
-   const ocultarMenu = document.querySelector('.mostrar-menu');
-   
-   if(ocultarMenu) {
-       ocultarMenu.classList.remove('mostrar-menu');
-   } else {
-       const mostrarMenu = document.querySelector('.navegacion-header');
-       mostrarMenu.classList.add('mostrar-menu');
-   }
+document.addEventListener('DOMContentLoaded', function() {
+    DOMContentLoaded();
 });
+
+function DOMContentLoaded() {
+
+    // Menu del MediaQuerie
+    menuNav();
+
+    // Insertar el Resumen de los posts en la seccion Publicaciones
+    resumeArticle();
+
+    
+
+}
+
+function menuNav() {
+    const menu = document.querySelector('.menu');
+
+    menu.addEventListener('click', e => {
+        const ocultarMenu = document.querySelector('.mostrar-menu');
+    
+        if(ocultarMenu) {
+            ocultarMenu.classList.remove('mostrar-menu');
+        } else {
+            const mostrarMenu = document.querySelector('.navegacion-header');
+            mostrarMenu.classList.add('mostrar-menu');
+        }
+    });
+}
+
+
+const contentArticle = document.querySelector('.resume-articles');
+
+const titleArticle = document.querySelector('.article-title').innerHTML;
+
+console.log(titleArticle)
